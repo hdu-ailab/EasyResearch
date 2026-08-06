39ef432db3de651843147a1db80c4c44cb28ffda
ChengYaoyang <chengyaoyang2001@gmail.com>
2026-08-06 21:22:56 +0800

    feat(webui): apply font sizes synchronously from localStorage (ADR-029)

--- src/webui/src/main.tsx
+++ src/webui/src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import { readPreferences } from "./preferences";
+import { applyFontPreferences } from "./webui-fonts";
 import "./index.css";
+
+applyFontPreferences(readPreferences(window.localStorage, () => navigator.language));
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
