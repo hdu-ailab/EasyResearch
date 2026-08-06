2f5e8a342c3d7932d0ecbd30fb602ecab54872b3
ChengYaoyang <chengyaoyang2001@gmail.com>
2026-08-06 21:26:47 +0800

    feat(webui): dependency-free EN/ZH-CN i18n core (ADR-029)

--- src/webui/src/main.tsx
+++ src/webui/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import { I18nProvider } from "./i18n/I18nProvider";
 import { readPreferences } from "./preferences";
 import { applyFontPreferences } from "./webui-fonts";
 import "./index.css";
@@ -9,6 +10,8 @@ applyFontPreferences(readPreferences(window.localStorage, () => navigator.langua
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <I18nProvider>
+      <App />
+    </I18nProvider>
   </StrictMode>,
 );
